25056be9df0311ecea1a428c537423f614571f91
Jigs6720 <jigar.infynno@gmail.com>
2023-04-13 13:00:20 +0530

    Helper file updated changes

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,7 +3,7 @@ import Toast from 'react-native-toast-message';
 import NetInfo from '@react-native-community/netinfo';
 import {CommonActions} from '@react-navigation/native';
 
-export function navigationWithStackClear(ScreenName, navigation) {
+export function navigationWithStackClear(ScreenName:string, navigation:any) {
   navigation.dispatch(
     CommonActions.reset({
       index: 1,
@@ -12,23 +12,23 @@ export function navigationWithStackClear(ScreenName, navigation) {
   );
 }
 
-export function navigationToScreen(ScreenName, navigation, passObject) {
+export function navigationToScreen(ScreenName:string, navigation:any, passObject:any) {
   navigation.navigate(ScreenName, passObject);
 }
 
-export function ReplaceToScreen(ScreenName, navigation, passObject) {
+export function ReplaceToScreen(ScreenName:string, navigation:any, passObject:any) {
   navigation.replace(ScreenName, passObject);
 }
 
-export function showAlert(title, message) {
+export function showAlert(title:string, message:string) {
   Alert.alert(title, message);
 }
 
-export function goBack(navigation, num = 1) {
+export function goBack(navigation:any, num = 1) {
   navigation.pop(num);
 }
 
-export function resetNavigation(props, initialScreeName) {
+export function resetNavigation(navigation:any, initialScreeName:string) {
   navigation.dispatch(
     CommonActions.reset({
       index: 1,
@@ -37,7 +37,7 @@ export function resetNavigation(props, initialScreeName) {
   );
 }
 
-export function showToast_success(title, message, _onPress = () => {}) {
+export function showToast_success(title:string, message:string, _onPress = () => {}) {
   if (message) {
     return Toast.show({
       type: 'success',
@@ -48,7 +48,7 @@ export function showToast_success(title, message, _onPress = () => {}) {
   }
 }
 
-export function showToast_error(title, message, _onPress = () => {}) {
+export function showToast_error(title:string, message:string, _onPress = () => {}) {
   if (message) {
     return Toast.show({
       type: 'error',
@@ -59,7 +59,7 @@ export function showToast_error(title, message, _onPress = () => {}) {
   }
 }
 
-export function showToast_info(title, message, _onPress = () => {}) {
+export function showToast_info(title:string, message:string, _onPress = () => {}) {
   if (message) {
     return Toast.show({
       type: 'info',
